b439e793b634c8467672d2531a06b34942d750cf
ngarimwenda@gmail.com <ngarimwenda@gmail.com>
2024-09-25 21:37:18 +0300

    bootstrap client

--- disk-reader/static/stream.mjs
+++ disk-reader/static/stream.mjs
@@ -1,6 +1,33 @@
-function getFileId() {
-    const [, id] = window.location.pathname.split("/");
-    return id;
+const env = (() => {
+    const [, route, id] = window.location.pathname.split("/");
+    return route == "open" ? { route, id } : null;
+})();
+
+if (env == null) {
+    throw Error("problem loading executing env");
 }
 
-console.log("File Id ", getFileId());
+async function pingStreamServer() {
+    const { route, id } = env;
+    let message;
+    try {
+        message = await fetch(`${window.location.origin}/stream/${id}`);
+    } catch (e) {
+        console.error("something went wrong");
+    }
+    console.log("message from server ", message);
+}
+
+async function main() {
+    console.log(window.location);
+    await pingStreamServer();
+}
+
+main()
+    .then(() => {
+        console.log("execution finished");
+    })
+    .catch((e) => {
+        console.log("problem during execution");
+        console.error(e);
+    });
